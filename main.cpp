627d60b0ffc3f0cd95c32c557b15de585350114d
DilsThik <Disblocker.just@gmail.com>
2023-10-19 21:32:24 +0300

    remove ini

--- main.cpp
+++ main.cpp
@@ -49,6 +49,7 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.IniFilename = nullptr;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
     ImGui::StyleColorsDark();
